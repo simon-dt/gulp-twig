63f23c155e84753043077850680c00e925f0aada
Hasan Aydoğdu <hsnaydd@gmail.com>
2019-11-14 17:36:56 +0300

    Refactor & format code

--- index.js
+++ index.js
@@ -1,107 +1,103 @@
-var through2 = require('through2');
-var rext = require('replace-ext');
-var log = require('fancy-log');
-var PluginError = require('plugin-error');
+const through2 = require("through2");
+const rext = require("replace-ext");
+const log = require("fancy-log");
+const PluginError = require("plugin-error");
 
-const PLUGIN_NAME = 'gulp-twig';
+const PLUGIN_NAME = "gulp-twig";
 
-module.exports = function (options) {
-    'use strict';
-    options = Object.assign({}, {
-      changeExt: true,
-      extname: '.html',
-      useFileContents: false,
-
-    }, options || {});
+module.exports = function(options) {
+    "use strict";
 
     function modifyContents(file, encoding, callback) {
-        var data = file.data || Object.assign({}, options.data);
-
         if (file.isNull()) {
             return callback(null, file);
         }
 
         if (file.isStream()) {
-            return callback(new PluginError(PLUGIN_NAME, 'Streaming not supported!'));
+            return callback(
+                new PluginError(PLUGIN_NAME, "Streaming not supported!")
+            );
         }
 
-        data._file   = file;
-        if(options.changeExt === false || options.extname === true){
-            data._target = {
-                 path: file.path,
-                 relative: file.relative
-             }
-        }else{
-            data._target = {
-                path: rext(file.path, options.extname || ''),
-                relative: rext(file.relative, options.extname || '')
-            }
-        }
+        const {
+            changeExt = true,
+            extname = ".html",
+            useFileContents = false,
+            twigParameters = {},
+            data: optionsData,
+            cache,
+            functions,
+            filters,
+            extend,
+            errorLogToConsole,
+            onError
+        } = options || {};
 
-        var Twig = require('twig'),
-            twig = Twig.twig,
-            twigOpts = {
-                path: file.path,
-                async: false
-            },
-            template;
+        const data = file.data || optionsData || {};
 
-        if (options.debug !== undefined) {
-            twigOpts.debug = options.debug;
-        }
-        if (options.trace !== undefined) {
-            twigOpts.trace = options.trace;
-        }
-        if (options.base !== undefined) {
-            twigOpts.base = options.base;
-        }
-        if (options.namespaces !== undefined) {
-            twigOpts.namespaces = options.namespaces;
-        }
-        if (options.cache !== true) {
+        const target =
+            changeExt === false || extname === true
+                ? {
+                      path: file.path,
+                      relative: file.relative
+                  }
+                : {
+                      path: rext(file.path, extname || ""),
+                      relative: rext(file.relative, extname || "")
+                  };
+
+        const Twig = require("twig");
+        const { twig } = Twig;
+
+        if (cache !== true) {
             Twig.cache(false);
         }
 
-        if (options.functions) {
-            options.functions.forEach(function (func) {
+        if (functions) {
+            functions.forEach(function(func) {
                 Twig.extendFunction(func.name, func.func);
             });
         }
 
-        if (options.filters) {
-            options.filters.forEach(function (filter) {
+        if (filters) {
+            filters.forEach(function(filter) {
                 Twig.extendFilter(filter.name, filter.func);
             });
         }
 
-        if(options.extend) {
-            Twig.extend(options.extend);
-            delete options.extend;
+        if (extend) {
+            Twig.extend(extend);
         }
 
-        if (options.useFileContents) {
-          var fileContents = file.contents.toString();
-          twigOpts.data = fileContents
-        }
-
-        template = twig(twigOpts);
+        const template = twig({
+            ...twigParameters,
+            async: false,
+            path: file.path,
+            data: useFileContents ? file.contents.toString() : undefined
+        });
 
         try {
-            file.contents = new Buffer(template.render(data));
-        }catch(e){
-            if (options.errorLogToConsole) {
-                log(PLUGIN_NAME + ' ' + e);
+            file.contents = new Buffer.from(
+                template.render({
+                    ...data,
+                    _target: target,
+                    _file: file
+                })
+            );
+        } catch (e) {
+            if (errorLogToConsole) {
+                log(PLUGIN_NAME + " " + e);
                 return callback();
             }
 
-            if (typeof options.onError === 'function') {
-                options.onError(e);
+            if (typeof onError === "function") {
+                onError(e);
                 return callback();
             }
             return callback(new PluginError(PLUGIN_NAME, e));
         }
 
-        file.path = data._target.path;
+        file.path = target.path;
         callback(null, file);
     }
 
